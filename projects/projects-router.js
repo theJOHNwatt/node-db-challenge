0f762bd4dee6eb8cb020a4a69ec4e328261c8d82
theJOHNwatt <j.watt10@gmail.com>
2019-11-08 12:08:02 -0800

    mvp completed, tested all endpoints

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -1,6 +1,79 @@
 const express = require('express');
-const db = require('../data/db-config.js');
-
+const db = require('./projects-model.js');
 const router = express.Router();
+
+router.get('/', (req,res) => {
+    db.getProjects()
+        .then(proj => {
+            proj.forEach(item => {
+                if (item.completed) {
+                    item.completed = 'true'
+                } else {
+                    item.completed = 'false'
+                }
+            })
+            res.status(201).json(proj)
+        })
+        .catch(err => {
+            res.status(500).json({error: 'Error getting projects'})
+        })
+})
+
+router.get('/resources', (req,res) => {
+    db.getResources()
+    .then(reso => {
+        res.status(201).json(reso)
+    })
+    .catch(err => {
+        res.status(500).json({error:'Could not find resources'})
+    })
+})
+
+router.get('/tasks', (req, res) => {
+    db.getTasks()
+        .then(task => {
+            task.forEach(item => {
+                if (item.completed) {
+                    item.completed = 'true'
+                } else {
+                    item.completed = 'false'
+                }
+            })
+            res.status(201).json(task)
+        })
+        .catch(err => {
+            res.status(500).json({error:'Error getting tasks'})
+        })
+})
+
+router.post('/', (req, res) => {
+    db.addProject(req.body)
+        .then(proj => {
+            res.status(201).json(proj)
+        })
+        .catch(err => {
+            res.status(500).json({error: 'Error adding project'})
+        })
+})
+
+router.post('/tasks', (req, res) => {
+    db.addTask(req.body)
+        .then(proj => {
+            res.status(201).json(proj)
+        })
+        .catch(err => {
+            res.status(500).json({error: 'Error adding task'})
+        })
+})
+
+router.post('/resources', (req, res) => {
+    db.addResource(req.body)
+        .then(resource => {
+            res.status(201).json(resource)
+        })
+        .catch(err => {
+            res.status(500).json({error: 'Error adding resource: '})
+        })
+})
 
 module.exports = router;

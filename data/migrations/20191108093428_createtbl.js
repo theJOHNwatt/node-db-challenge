2784eda0b17c9edb82ef5b45fd46b6849e599a15
theJOHNwatt <j.watt10@gmail.com>
2019-11-08 10:42:05 -0800

    tables created, seeds created

--- data/migrations/20191108093428_createtbl.js
+++ data/migrations/20191108093428_createtbl.js
@@ -1,8 +1,28 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('projects', tbl => {
+        tbl.increments();
+        tbl.string('name').notNullable();
+        tbl.string('description', 255);
+        tbl.boolean('completed')
+    })
+    .createTable('resource', tbl => {
+        tbl.increments();
+        tbl.string('name').notNullable();
+        tbl.string('description', 255);
+        tbl.unique('name');
+    })
+    .createTable('tasks', tbl => {
+        tbl.increments();
+        tbl.string('description', 255).notNullable();
+        tbl.boolean('completed')
+    })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+  .dropTableIfExists('projects')
+  .dropTableIfExists('resource')
+  .dropTableIfExists('tasks')
 };
